2c8c7138b46cd3a8605a46417911d7c6583826e0
Julie Mendez <juliemendez@julies-mbp.lan>
2020-06-12 18:25:05 -0500

    Final files for presentation

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Project curtosy of tutorial from Dave Ed on YouTube: https://www.youtube.com/watch?v=bfaPnlYE8Jo 
+
 const moonPath = 
 "M166,334.000272 C74.3207315,334.000272 0,259.679268 0,168 C0,76.3207315 106.21875,-11.4863281 166,2 C167.110911,2.2506156 170.053134,2.33504854 169.098371,2.33504854 C167.118223,2.33504854 84.6908878,79.8924855 84.6908878,168 C84.6908878,258.523029 169.381776,334.000272 169.381776,333.964651 C169.381776,331.924333 167.156239,334.000272 166,334.000272 Z"
 
@@ -12,7 +14,6 @@ let toggle = false;
 //Ability to click on the sun
 darkMode.addEventListener('click', () => {
 
-//we need to use anime.js
 // Here we set up the Timeline
 const timeline = anime.timeline({
     duration : 750,
@@ -35,8 +36,8 @@ timeline.add ({
     //color changes toggles between light and dark text
     .add({
         targets: "section",
-        backgroundColor: toggle ? "rgb(255, 255,255)" : "rgb(22,22,22)"
-        //color: toggle ? "rgb(22,22,22)" : "rgb(255,255,255)"
+        backgroundColor: toggle ? "rgb(255, 255,255)" : "rgb(22,22,22)",
+        color: toggle ? "rgb(22,22,22)" : "rgb(255,255,255)"
 
     },
     "-= 700"
